1610e7c2968f417ba25e1ba2af3e9000e0127653
AnJuHwan <wnghks0531@gmail.com>
2021-11-18 17:20:19 +0900

    MealItem ui

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './MealItem.module.css';
+
+const MealItem = ({ name, description, price }) => {
+  const changePrice = `$${price.toFixed(2)}`;
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.price}>{changePrice}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
